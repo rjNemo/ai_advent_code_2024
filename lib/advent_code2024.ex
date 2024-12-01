aa1d1ac20d9c9d4e5735a68ad0950bd64510c9ed
Ruidy <ruidy.nemausat@gmail.com>
2024-12-01 09:46:42 +0100

    project scaffold

---
+++ lib/advent_code2024.ex
@@ -0,0 +1,18 @@
+defmodule AdventCode2024 do
+  @moduledoc """
+  Documentation for `AdventCode2024`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventCode2024.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/advent_code2024_test.exs
@@ -0,0 +1,8 @@
+defmodule AdventCode2024Test do
+  use ExUnit.Case
+  doctest AdventCode2024
+
+  test "greets the world" do
+    assert AdventCode2024.hello() == :world
+  end
+end
